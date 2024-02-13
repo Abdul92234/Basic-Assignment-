8d6092cb0543106cc628789aebfe5adcf8deb620
Abdul92234 <159756517+Abdul92234@users.noreply.github.com>
2024-02-13 12:20:16 +0530

    Create CircumferenceOfCircle.cpp

---
+++ CircumferenceOfCircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int radius = 10;
+  float circumference = 2*3.14*r;
+  cout<<"The circumference of circle with radius 10 is "<<circumference;
+  return 0;
+}
